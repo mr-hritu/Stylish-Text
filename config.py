59bc17449836b9335d2e42136938f32e5a02b44b
Hritu-king <87654408+Hritu-king@users.noreply.github.com>
2023-06-23 12:17:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ class Config(object):
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "6114945822:AAEWOTZuIjoE5GgEzmLn5_Qu8baDS4zAYmk")
       API_ID = int(os.environ.get("API_ID", 29616312))
       API_HASH = os.environ.get("API_HASH" , default="dd1a05ab4c47a5a037cc067cf4bded27")
-      OWNER_ID = os.environ.get("OWNER_ID" , default="prime_hritu") 
-
+      OWNER_ID = int(os.environ.get("OWNER_ID" , default="6262157003"))
