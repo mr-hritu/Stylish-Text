2c8a53c0840269e7fd8e4dda134f296d37dc1252
Hritu-king <87654408+Hritu-king@users.noreply.github.com>
2023-06-23 12:34:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,7 +16,7 @@ async def start(c, m):
 💡 ** I am Stylish Font Bot**
 
 
-**💟 Send Me Some Text To Change It's Font 💟**
+**💟 Send Me Some Text To Change It's Font**
 
 
 **🥺 Please Join : @Private_Bots 🌺**
