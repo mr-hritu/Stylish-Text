8fea052b662d6badad5ded63f39d77fbcea74213
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-06 13:22:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config(object):
 
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "6300910303:AAGyg1ckI_SR2YVw_xlDIIIZ1C_qukWGjuw")
-      API_ID = int(os.environ.get("API_ID", 28888037))
-      API_HASH = os.environ.get("API_HASH" , default="9fbe164b5591df05fbd8577e3b1d6d21")
+      API_ID = int(os.environ.get("API_ID", 5321008))
+      API_HASH = os.environ.get("API_HASH" , default="8a633e8f5259a852ec85d4e8cfc31e67")
       OWNER_ID = int(os.environ.get("OWNER_ID" , default="5190902724"))
